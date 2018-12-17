d3e084c8a9f594fb48a02895db85730e998cab3c
Lucas Ramage <lramage@cyberninjas.com>
2018-12-17 17:38:58 +0000

    bump MAX_POOL_SIZE; indent comments where needed

--- src/libnss_pool.c
+++ src/libnss_pool.c
@@ -11,10 +11,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//This defines the maximal size of the pool stored in /etc/pool-passwd
-#define MAX_POOL_SIZE 20
+// This defines the maximal size of the pool stored in /etc/pool-passwd
+#define MAX_POOL_SIZE 90
 
-//poolPasswd file
+// poolPasswd file
 #define POOLPASSWD "/etc/pool-passwd"
 
 
@@ -39,28 +39,40 @@ _nss_pool_getpwnam_r( const char *name,
 	while ( (it=fgetpwent(fd)) != NULL) {
 
 		fprintf(log,"Read line: %s\n",it->pw_name);
-		if(!strcmp(name,it->pw_name) && !(strcmp("x",it->pw_passwd)) && !created ) {
+		if(!strcmp(name,it->pw_name) && !(strcmp("x",it->pw_passwd)) 
+                   && !created )
+                {
 			fprintf(log,"Match %s \n",it->pw_name);
 			*p=*it;
-			//entry was there no need to process further
+			// Entry was there; no need to process further
 			fclose(log);
 			return NSS_STATUS_SUCCESS;
 			
 		}
-		//if passwd set to ! then it means that entry is empty and can be used for new user
+		// If passwd set to ! then it means that entry is empty
+                // and can be used for new user
 		else if(!strcmp("!",it->pw_passwd) && !created ) {
-			fprintf(log,"new %s \n",it->pw_name);
-			strcpy(it->pw_name,name); //this may be an issue if previous login name was shorter then new one
+			fprintf(log,"new %s \n",it
+                        // FIXME this may be an issue if previous login name 
+                        // was shorter then new one->pw_name);
+			strcpy(it->pw_name,name); 
 			fprintf(log,"new %s \n",it->pw_name);
 			strcpy(it->pw_passwd,"x");
 			*p=*it;
 			created=1;
 		}
 		
-		fprintf(log,"snprintf, when strlen(buffer)=%d and buflen=%d\n",(int)strlen(buffer),(int)buflen);
-		fprintf(log,"Adding to pool-passwd: %s:%s:%d:%d:%s:%s:%s\n",it->pw_name,it->pw_passwd,it->pw_uid,it->pw_gid,it->pw_gecos,it->pw_dir,it->pw_shell);
+		fprintf(log,"snprintf, when strlen(buffer)=%d and buflen=%d\n",
+                       (int)strlen(buffer),(int)buflen);
 
-		int howLong=snprintf(buffer+strlen(buffer),buflen,"%s:%s:%d:%d:%s:%s:%s\n",it->pw_name,it->pw_passwd,it->pw_uid,it->pw_gid,it->pw_gecos,it->pw_dir,it->pw_shell);
+		fprintf(log,"Adding to pool-passwd: %s:%s:%d:%d:%s:%s:%s\n",
+                       it->pw_name,it->pw_passwd,it->pw_uid,it->pw_gid,
+                       it->pw_gecos,it->pw_dir,it->pw_shell);
+
+		int howLong=snprintf(buffer+strlen(buffer),buflen,
+                                    "%s:%s:%d:%d:%s:%s:%s\n",it->pw_name,
+                                    it->pw_passwd,it->pw_uid,it->pw_gid,
+                                    it->pw_gecos,it->pw_dir,it->pw_shell);
 
 		fprintf(log,"checking buffer\n");
 		if(howLong+1>buflen) {
@@ -76,14 +88,17 @@ _nss_pool_getpwnam_r( const char *name,
 		return NSS_STATUS_NOTFOUND;
 	}
 
-	//If we are here it means that new user was created (name changed) in pool
+	// If we are here it means that new user
+        // was created (name changed) in pool
 	fd=fopen(POOLPASSWD,"w");
 	fprintf(fd,"%s",buffer);
 	fclose(fd);
 
 	fclose(log);
-	return NSS_STATUS_TRYAGAIN; //Means that there are no more entries, we don't implement users enumeration
-	
+
+        // Means that there are no more entries, 
+        // we don't implement users enumeration
+	return NSS_STATUS_TRYAGAIN; 
 }
 
 enum nss_status
